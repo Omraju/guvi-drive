1a2f6f38259c886fe0a9c241b0b5103fa6236d11
Om Raju <omraju006@gmail.com>
2022-11-06 11:59:03 +0530

    enter

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 <script>
 $(function () {
-$('form#Myform1').on('submit', function (event) {
-$.ajax({
-type: 'post',
-url: 'guvi-drive/php/login.php',
-data: $('form').serialize(),
-success: function (data) {
-$("#data").html(data);
-}
-});
-event.preventDefault();
-})
+	$('form#Myform').on('login', function (event) {
+		$.ajax({
+			type: 'post',
+			url: 'index.php',
+			data: $('form').serialize(),
+			success: function (data) {
+			  $("#data").html(data);
+			}
+		});
+		event.preventDefault();
+	})
 });
 </script>
--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,15 @@
 <script>
 $(function () {
-$('form#Myform2').on('submit', function (event) {
-$.ajax({
-type: 'post',
-url: 'profile.php',
-data: $('form').serialize(),
-success: function (data) {
-$("#data").html(data);
-}
-});
-event.preventDefault();
-});
+	$('form#Myform').on('login', function (event) {
+		$.ajax({
+			type: 'post',
+			url: 'register.php',
+			data: $('form').serialize(),
+			success: function (data) {
+			  $("#data").html(data);
+			}
+		});
+		event.preventDefault();
+	})
 });
 </script>
